3357e2e11c4f739b648ec364f1fb34964ef96bc6
Chris Coppens <chris.coppens@topic.nl>
2015-10-02 15:10:38 +0200

    Add script to overwrite build configuration with the required settings for meta-topic-desktop. Primarily to be used by Jenkins automated build.

---
+++ scripts/setup-desktop-build.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+set -e
+if [ ! -z "$1" ]
+then
+  BUILDDIR="$1"
+else
+  BUILDDIR=build
+fi
+
+if [ ! -d oe-core ]
+then
+	echo "Please run this as 'meta-topic-desktop/scripts/setup-desktop-build.sh' from the top directory."
+	exit 1
+fi
+
+if [ ! -d ${BUILDDIR}/conf ]
+then
+	mkdir -p ${BUILDDIR}/conf
+fi
+cp -rp meta-topic-desktop/scripts/templates/build/* ${BUILDDIR}/
+
+echo "Build configuration has been overwritten (profile, conf/bblayers.conf, conf/local.conf)." 
