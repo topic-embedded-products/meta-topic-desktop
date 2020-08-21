48fc25a5181768aaaf7a8406a5538850e40883ef
Mike Looijmans <mike.looijmans@topic.nl>
2020-08-21 13:14:55 +0200

    xdpzu7: Fix that WIC images boot from SD card
    The wic images would not boot on the xdpzu7 because of the SD/wifi pinmux
    being set to "wifi". Implement the fix from topic-platform for that.

--- scripts/install_to_sd_desktop-image-xdpzu7.sh
+++ scripts/install_to_sd_desktop-image-xdpzu7.sh
@@ -1,5 +1,3 @@
 IMAGE=desktop-image
 MACHINE=xdpzu7
-# Make the SD card work by using an alternative devicetree.
-ROOTFS_FIXUP_COMMANDS="ln -f -s devicetree/zynqmp-topic-miamimp-xilinx-xdp-sd-usbhost.dtb boot/system.dtb"
 source `dirname $0`/install_to_sd_common.sh
